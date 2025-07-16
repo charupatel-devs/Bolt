8737193cf90a4177b60fc13d559bc4051d4a6f28
Charu Patel <90954539+Sunshadeshine@users.noreply.github.com>
2025-07-17 00:17:56 +0530

    updated

--- client/src/services_hooks/api.js
+++ client/src/services_hooks/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create axios instance with base configuration
 const api = axios.create({
-  baseURL: { REACT_APP_API_URL },
+  baseURL: "https://bolt-pup2.onrender.com/api",
   timeout: 10000,
   withCredentials: true,
   headers: {
